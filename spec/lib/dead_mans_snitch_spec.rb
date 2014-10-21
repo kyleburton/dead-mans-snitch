4ea8dfe7ca160e023cd723076fb925a9cd0de8b7
Harald Wartig <hwartig@gmail.com>
2014-10-21 10:55:37 +0200

    Add DeadMansSnitch.report_with_time.
    This will allow one to pass a block which will be executed before
    reporting to dead mans snitch. This method will also report the running
    time of the block and the result to dead mans snitch.

--- spec/lib/dead_mans_snitch_spec.rb
+++ spec/lib/dead_mans_snitch_spec.rb
@@ -38,4 +38,47 @@ describe DeadMansSnitch do
       it { is_expected.to be_falsey }
     end
   end
+
+  describe '.report_with_time' do
+    let(:block) { nil }
+
+    subject { described_class.report_with_time(snitch_id, &block) }
+
+    context 'when no block was passed' do
+      specify { expect { subject }.to raise_error(LocalJumpError) }
+    end
+
+    context 'when a block was passed' do
+      let(:block) { -> { 'result message' } }
+
+      it 'yields control' do
+        expect { |b| described_class.report_with_time(snitch_id, &b) }.to yield_control
+      end
+
+      it 'passes the result of the block to DMS' do
+        expect(DeadMansSnitch).to receive(:report).with(snitch_id, 'Took: 0h 0m 0s, result message').and_return('http result')
+        expect(subject).to eq('http result')
+      end
+
+      specify { expect { subject }.to_not raise_error }
+    end
+  end
+
+  describe DeadMansSnitch::Utils do
+    describe '.seconds_to_human' do
+      subject { described_class.seconds_to_human(seconds) }
+
+      context 'when passed a invalid number of seconds' do
+        let(:seconds) { -1000 }
+
+        specify { expect { subject }.to raise_error(ArgumentError) }
+      end
+
+      context 'when passed a positive number' do
+        let(:seconds) { 4201 }
+
+        it { is_expected.to eq('1h 10m 1s') }
+      end
+    end
+  end
 end
